709df0f025e709b7e57b2c423e2fa43ce2cfa309
Zhang-dev <55280516+Zhang-dev@users.noreply.github.com>
2019-11-06 16:22:59 +0100

    img url modified

--- js/data/items.js
+++ js/data/items.js
@@ -6,7 +6,7 @@ export const items = [
     description:
       "Die Sohle mit der innovativen BOOST™-Technologie dämpft beim Laufen jeden deiner Schritte. So macht Joggen Spaß – mit dem coolen Laufschuh ",
     price: "80.90",
-    image: "../../images/01.jpg",
+    image: "images/01.jpg",
     size: [36, 37, 38, 39, 40, 43, 45],
     color: ["white", "black", "gray"]
   },
@@ -17,7 +17,7 @@ export const items = [
     description:
       "Das atmungsaktive Meshobermaterial vollendet das Komfortdesign. Alles, was sie brauchst: der Laufschuh von Core Sport.",
     price: "40.90",
-    image: "../../images/02.jpg",
+    image: "images/02.jpg",
     size: [36, 40, 43],
     color: ["white", "pink"]
   },
@@ -28,7 +28,7 @@ export const items = [
     description:
       "Damit er jeden deiner Schritte möglichst natürlich abrollen kannst, ist die Gummilaufsohle mit Flexkerben ausgestattet. Ganz gleich, ob du auf Zeit oder Ausdauer trainierst",
     price: "45.90",
-    image: "../../images/03.jpg",
+    image: "images/03.jpg",
     size: [36, 37, 38, 39],
     color: ["white", "black", "gray"]
   },
@@ -39,7 +39,7 @@ export const items = [
     description:
       "Ein Sportliches Outfit gelingt mit Leggings, Sport-BH und lockerem Tanktop. Ob zum Work-out oder in der Freizeit ",
     price: "64.90",
-    image: "../../images/04.jpg",
+    image: "images/04.jpg",
     size: [37, 38, 39],
     color: ["white", "black"]
   },
@@ -50,7 +50,7 @@ export const items = [
     description:
       "Mit seinem klassischen Look passt der Sneaker perfekt zum angesagten College-Style. Aber die Schuhe sehen nicht nur cool aus, sondern bieten mit ihrem gepolsterten Einstieg, den Perforationen zum Lüften und dem weichen Leder auch ein traumhaft bequemes Tragegefühl. Als Hingucker dienen die seitlichen Streifen und die Logos auf der Seite und der Zunge. ",
     price: "79.99",
-    image: "../../images/05.jpg",
+    image: "images/05.jpg",
     size: [39, 40, 41, 42],
     color: ["weiß-grün", "weiß-schwarz"]
   },
@@ -61,7 +61,7 @@ export const items = [
     description:
       "Die robuste Gummisohle hat mit ihrem Profil jedes Terrain von City bis Wald im Griff. Durch den gepolsterten Schaft und die praktische Schnürung sitzt der Nubukstiefel zudem richtig toll. Und das Beste daran: Der clevere Outdoorschuh ist dank der versiegelten Nähte wasserfest. Genau dein Fall: die Schnürstiefel »Premium Boot L/F« von Timberland.",
     price: "199.99",
-    image: "../../images/06.jpg",
+    image: "images/06.jpg",
     size: [37, 38, 40, 42],
     color: ["schwarz", "wheat"]
   },
@@ -72,7 +72,7 @@ export const items = [
     description:
       "Das markentypische Luftpolster in der Sohle fehlt auch bei diesem Modell nicht und sorgt optisch für einen stylischen Akzent. Mit der Schnürung kannst du die Passform dann individuell deinem Fuß anpassen.",
     price: "129.99",
-    image: "../../images/07.jpg",
+    image: "images/07.jpg",
     size: [42, 43, 4, 45, 46, 47],
     color: ["schwarz", "weiß"]
   },
@@ -83,7 +83,7 @@ export const items = [
     description:
       "Mehr Stilvermögen geht nicht: mit dieser eleganten Anzugweste von Bruno Banani. Der Rücken aus edlem Satinfutter ist mit dezentem Logoprint verziert. Dekorative Seitentaschen und die gepflegte 2-Knopf-Form runden das Design der charismatischen Weste ausdrucksstark ab. Der erhöhte Stretchanteil der komfortablen Materialmischung garantiert ein angenehmes Tragegefühl.",
     price: "59.99",
-    image: "../../images/08.jpg",
+    image: "images/08.jpg",
     size: [34, 36, 38, 40, 42, 44],
     color: []
   },
@@ -94,7 +94,7 @@ export const items = [
     description:
       "Mit diesem Teil verführen Sie garantiert jeden Mann. Das Spitzen Top kreiert einen verführerischen Anblick den niemand widerstehen kann. Komplett aus zarter Spitze umschmeichelt es sanft die Haut und lässt diese ebenso reizvoll durchscheinen. Vorne kann es mit filigranen Knöpfen in edler Perlenoptik einfach geöffnet werden. Die sinnlich anmutende Optik des Spitzen-Hemdchens verführt mit allen Mitteln der Kunst. Aus 90% Polyamid, 10% Elasthan.",
     price: "29.99",
-    image: "../../images/09.jpg",
+    image: "images/09.jpg",
     size: [34, 36, 38, 40, 42],
     color: []
   }
@@ -107,7 +107,7 @@ export const items = [
     description:
       "Das Trainingstank performt, wenn du beim Sport dein Bestes gibst. Das innovative Feuchtigkeitsmanagement hält dich angenehm trocken, die Flachnähte verhindern unangenehme Reibung und in der lockeren Passform mit Racerback kannst du dich frei bewegen. Das Sporttop besitzt ein kleines Logodetail und ist eher schlicht gehalten.",
     price: "21.99",
-    image: "../../images/10.jpg",
+    image: "images/10.jpg",
     size: [52, 54, 56, 58],
     color: ["koralle", "schwarz"]
   },
@@ -118,7 +118,7 @@ export const items = [
     description:
       "Bestens gestylt zum Training: Mit dem Yogatanktop von Nike setzt du das im Handumdrehen um und erhältst zudem auch noch Support durch die funktionale Dri-FIT-Technologie. Sie stattet das tailliert geschnittene Oberteil mit atmungsaktiven und schnelltrocknenden Eigenschaften aus.",
     price: "19.99",
-    image: "../../images/11.jpg",
+    image: "images/11.jpg",
     size: [30, 32, 34, 36, 38, 40],
     color: ["lachs", "schwarz"]
   },
@@ -129,7 +129,7 @@ export const items = [
     description:
       "Kult für jeden Tag: Das T-Shirt ist ein absolutes Must-have für Freundinnen des lässigen Styles. Das Shirt ist schmal geschnitten und bietet angenehmen Tragekomfort. Ob für die Uni, zum Shopping oder für die nächste Party – das Logoshirt lässt sich super kombinieren.",
     price: "24.99",
-    image: "../../images/12.jpg",
+    image: "images/12.jpg",
     size: [32, 34, 36, 38, 40, 42],
     color: [
       "Batwing black/red",
@@ -145,7 +145,7 @@ export const items = [
     description:
       "Raffiniertes Design und feines Webkaro – das zeichnet den Rock aus. Der flache Bund trägt nicht auf und die Falten am unteren Rockteil schaffen eine charmante A-Linie. Der Karorock ist mit Webfutter unterlegt, das sorgt für ein besonders angenehmes Tragegefühl.",
     price: "34.99",
-    image: "../../images/13.jpg",
+    image: "images/13.jpg",
     size: [34, 36, 38],
     color: ["tobacco brown", "north atlantic", "black"]
   },
@@ -157,7 +157,7 @@ export const items = [
     description:
       "Einen soliden Fashionpartner mit Wohlfühlfaktor findest du in diesem süßen Strickkleid von Only. Mit seinem angesagten Farbblockdesign und seiner melierten Optik zeigt es, wie guter Stil funktioniert. Einen schönen Akzent setzt außerdem der Rundhalsausschnitt, der mit einer modischen Rollkante abschließt.",
     price: "29.99",
-    image: "../../images/14.jpg",
+    image: "images/14.jpg",
     size: [36, 38, 40, 42, 44],
     color: ["Mahogany Rose", "Night Sky"]
   },
@@ -169,7 +169,7 @@ export const items = [
     description:
       "Mit dem Feinstrickkleid kommt so schnell keine Langeweile auf. Zum einen sorgt das wilde Allovermuster immer wieder für ein spannendes Styling. Zum anderen besitzt das Kleid einen kuscheligen Rollkragen, den du nach Belieben abtrennen kannst. Dann bietet dir das körpernahe, anschmiegsame Damenkleid eine klassische Optik mit Rundhalsausschnitt.",
     price: "119.99",
-    image: "../../images/15.jpg",
+    image: "images/15.jpg",
     size: [34, 36, 38, 40, 42, 44, 46],
     color: []
   },
@@ -180,7 +180,7 @@ export const items = [
     title: "Only High-waist-Jeans, 5-Pocket-Design",
     description: "High-Waist-Jeans Paola",
     price: "34.99",
-    image: "../../images/16.jpg",
+    image: "images/16.jpg",
     size: [30, 32, 34],
     color: ["dark-blue-denim"]
   },
@@ -191,7 +191,7 @@ export const items = [
     description:
       "Culotte in Ankle-Länge für Frauen (unifarben, mit Knopf und Reißverschluss zum Schließen) aus leichtem Gewebe mit Stretch-Anteil, mit Eingrifftaschen vorne",
     price: "59.99",
-    image: "../../images/17.jpg",
+    image: "images/17.jpg",
     size: [32, 34, 36, 38, 40, 42, 44, 46],
     color: ["olivgrün", "blau"]
   },
@@ -202,7 +202,7 @@ export const items = [
     description:
       "Der Charme des Mantels lebt durch dessen zurückhaltende Eleganz. Das Glencheckmuster des Kurzmantels ergänzen der traditionelle Reverskragen und die 3-Knopf-Form. Praktisch: die zwei seitlichen Eingriffstaschen mit Druckknopf. Der gerade Modern-fit des Herrenmantels verläuft nahe der Silhouette und setzt den Oberkörper vorteilhaft in Szene.",
     price: "129.99",
-    image: "../../images/18.jpg",
+    image: "images/18.jpg",
     size: [44, 46, 48, 50, 52, 54, 56, 58],
     color: []
   },
@@ -213,7 +213,7 @@ export const items = [
     description:
       "Ein sympathischer Allrounder für jeden Tag: Der softe Herrenmantel begeistert mit einer angenehmen Wollmischung und einer kontrastfarbenen Innenausstattung. Die schlanke Form des Designermantels mit 3-Knopf-Verschluss und Reverskragen mit Knopfloch zeigen sich im klassischen Design mit moderner Note. Seitliche Eingrifftaschen wärmen an kälteren Tagen die Hände und der Gehschlitz auf der Rückseite erlaubt beste Bewegungsfreiheit.",
     price: "109.99",
-    image: "../../images/19.jpg",
+    image: "images/19.jpg",
     size: [44, 46, 48, 50, 52, 54, 56, 58],
     color: []
   },
@@ -224,7 +224,7 @@ export const items = [
     description:
       "Typisches Krokodil Logo Badge auf der Brust, Mit Rundhalsausschnitt.",
     price: "40.00",
-    image: "../../images/20.jpg",
+    image: "images/20.jpg",
     size: [2, 3, 4, 5, 6, 7, 8, 9],
     color: ["senf-gelb", "royalblau", "khaki", "grau-meliert", "schwarz"]
   },
@@ -235,7 +235,7 @@ export const items = [
     description:
       "Das Graphic Set-In Neck ist ein T-Shirt mit Power: Es punktet mit klassischer Passform. Was es aber so besonders macht, ist sein lässiger und stylischer Vintage-Look mit angesagtem Levi's® Batwing Logo-Print. So frisch kann Old School aussehen.",
     price: "19.95",
-    image: "../../images/21.jpg",
+    image: "images/21.jpg",
     size: [32, 34, 36, 38, 40],
     color: ["dress blues", "dunkelrot"]
   },
@@ -246,7 +246,7 @@ export const items = [
     description:
       "Diese Jeansjackekannst du auch noch tragen, wenn die Tage kühler werden! Sie ist am Torso mit kuscheligem und wärmendem Teddyplüsch gefüttert. Das weiche Fellimitat macht auch den besonderen Look der Denimjacke aus, denn auch der Kragen ist mit Teddyplüsch besetzt und an der durchgehenden Knopfleiste blitzt das softe Innenfutter hervor.",
     price: "139.95",
-    image: "../../images/22.jpg",
+    image: "images/22.jpg",
     size: [40, 42, 44, 46],
     color: ["beige", "stonebridge"]
   },
@@ -258,7 +258,7 @@ export const items = [
     description:
       "Stylisch gegen die Kälte: Der funktionale Steppparkaist dank des atmungsaktiven sowie wasser- und windabweisenden Materials die erste Wahl für diese Mission. Er ist mit einem hohen Wärmerrückhaltevermögen ausgestattet. Auch die gefütterte Kapuze mit abnehmbarem Fellimitatbesatz und hohem Stehkragen trägt hierzu bei.",
     price: "149.99",
-    image: "../../images/23.jpg",
+    image: "images/23.jpg",
     size: [44, 46, 48, 50, 52, 54, 60, 64],
     color: ["rauchblau", "schwarz"]
   },
@@ -268,7 +268,7 @@ export const items = [
     title: "Rundhalspullover mit Streifenoptik",
     description: "Hochwertige Qualität durch den Materialmix",
     price: "19.99",
-    image: "../../images/24.jpg",
+    image: "images/24.jpg",
     size: [40, 44, 48, 52, 56, 60, 64, 68, 70],
     color: ["anthrazit-marine-gestreift", "marine-weiß-gestreift"]
   },
@@ -279,7 +279,7 @@ export const items = [
     description:
       "Du suchst eine Hose, die du in der Freizeit und im Beruf tragen kannst? Mit der Chinohosehat deine Suche ein Ende. Die klassische Baumwollhose mit den Paspeltaschen hinten und praktischen Eingrifftaschen vorn kannst du mit einem stilvollen Gürtel und deinem legeren Businesshemd wunderbar für das Büro stylen.",
     price: "49.99",
-    image: "../../images/25.jpg",
+    image: "images/25.jpg",
     size: [29, 30, 31, 32, 33, 34, 35, 36],
     color: ["dunkelgrün", "grau", "olivgrün", "schwarz"]
   },
@@ -290,7 +290,7 @@ export const items = [
     description:
       "Eine lässige Lieblingshose! Die Jeans erhält ihren coolen Style durch die modische Waschung mit Sitzfalten-Optik. Auch der entspannte Bootcut-fit mit niedriger Leibhöhe und ausgestelltem Bein trägt zur legeren Optik der 5-Pocket-Denim bei. Die robuste Qualität mit Stretchanteil sorgt bei der Herrenjeans nicht nur für einen guten und bequemen Sitz.",
     price: "59.95",
-    image: "../../images/26.jpg",
+    image: "images/26.jpg",
     size: [30, 32, 34, 36, 38, 30, 42, 44],
     color: ["broken-arrow", "high-blue", "black"]
   },
@@ -301,7 +301,7 @@ export const items = [
     description:
       "Edelstahl-Teleskoprohr-Rückenschonend:Die höhenverstellbaren Teleskoprohre passen sich per Knopfdruck der Arbeitshöhe an. Staubbeutelwechsel-Anzeige: Zeigt den Füllgrad des Staubbeutels an.",
     price: "169.00",
-    image: "../../images/27.jpg",
+    image: "images/27.jpg",
     size: [],
     color: ["blau", "grau", "weiss"]
   },
@@ -312,7 +312,7 @@ export const items = [
       "Einbaukühlgefrierkombination HEKS17754GA2W, 177,2 cm hoch, 54 cm breit",
     description: "Energieeffizienzklasse A++ (A+++ bis D)",
     price: "399.99",
-    image: "../../images/28.jpg",
+    image: "images/28.jpg",
     size: [],
     color: []
   },
@@ -323,7 +323,7 @@ export const items = [
     description:
       "Mit dem Wok, der für alle Herdarten geeignet ist, findet sie am Kochen Spaß.",
     price: "89.90",
-    image: "../../images/29.jpg",
+    image: "images/29.jpg",
     size: [],
     color: []
   },
@@ -334,7 +334,7 @@ export const items = [
     description:
       "Dieser moderne Couchtisch ist mehr als ein stylischer Wohnmittelpunkt. Das moderne Möbel ist mit einer batteriebetriebenen LED-Beleuchtung ausgestattet, die einen ganzen Raum mit stimmungsvoller Atmosphäre erfüllt. Der stabile Korpus ist aus soliden Leichtbauplatten mit strapazierfähigen ABS-Kanten gefertigt. Eine gläserne Ablagefläche und der chromfarbene Mittelsockel verleihen dem Sofatisch seine edle Note.",
     price: "199.99",
-    image: "../../images/30.jpg",
+    image: "images/30.jpg",
     size: [],
     color: ["steinfarben", "kernbuche", "walnussfarben", "Sonoma eichefarben"]
   },
@@ -346,7 +346,7 @@ export const items = [
     description:
       "Überzeugt mit Funktionalität und im schicken Design: das Schlafsofa. Erhältlich ist das praktische Faltsofa in verschiedenen Farbvarianten und Größen. Zudem ist es frei im Raum stellbar und ermöglicht somit einfaches Integrieren in das bestehende Wohnkonzept. Ausgestattet ist es zudem mit Nierenkissen, die für einen dekorativen Akzent sorgen.",
     price: "649.99",
-    image: "../../images/31.jpg",
+    image: "images/31.jpg",
     size: [],
     color: ["flamingo", "türkis", "mango"]
   },
@@ -357,7 +357,7 @@ export const items = [
     description:
       "Mit klassischer Formsprache weiß das Lowboard zu begeistern. Das Möbelstück wurde geradlinig und symmetrisch verarbeitet und besticht durch die zeitlose Form. So eignet es sich hervorragend, um den Fernseher perfekt auszurichten, aber auch, um dezente Dekorationselemente zu präsentieren.",
     price: "279.99",
-    image: "../../images/32.jpg",
+    image: "images/32.jpg",
     size: [],
     color: ["Kernbuche teilmassiv", "Eiche teilmassiv"]
   },
@@ -368,7 +368,7 @@ export const items = [
     description:
       "Geeignet für vielfältigste Anwendungen für ihn und somit die optimale Ergänzung für jeden Haushalt",
     price: "54.99",
-    image: "../../images/33.jpg",
+    image: "images/33.jpg",
     size: [],
     color: ["grün"]
   },
@@ -379,7 +379,7 @@ export const items = [
     description:
       "Kraftvoll. Widerstandsfähig. Langlebig. Mit dem Hammer erleichert ihm alle Arte von Handwerkerarbeiten",
     price: "74.99",
-    image: "../../images/34.jpg",
+    image: "images/34.jpg",
     size: [],
     color: []
   },
@@ -391,7 +391,7 @@ export const items = [
     description:
       "Nordisches Fichtenholz im Steck- und Schraubsystem mit Eckeinstieg, Inkl. Leuchte »Modern« und Zubehörset »Classic«.",
     price: "1169.99",
-    image: "../../images/35.jpg",
+    image: "images/35.jpg",
     size: [],
     color: ["natur"]
   },
@@ -402,7 +402,7 @@ export const items = [
     description:
       "Eleganter Designklassiker. Der Lederimitatrocksetzt die figurbetonte Miniform mit eleganten französischen Taschen und einem metallisch glänzenden Reißverschluss im Rückteil um. Mehr noch: Veredelt wird der knackige Rock mit dem Stretchanteil durch eine stilvolle Veloursoptik.",
     price: "29.99",
-    image: "../../images/36.jpg",
+    image: "images/36.jpg",
     size: [34, 36, 38, 40, 42],
     color: ["cognac", "bungee cord", "ponderosa pine", "black", "pink"]
   },
@@ -413,7 +413,7 @@ export const items = [
     description:
       "Der ausgestellte Rock präsentiert sich als unschlagbares Kombiwunder. Das zurückhaltende, cleane Design wird von einem nahtlos angesetzten, flachen Bund bestens unterstrichen. Weich fließender, griffiger Jersey erweist sich als das perfekte Material, um dem ausgestellten Bahnenrock echten Schwung zu verleihen.",
     price: "49.99",
-    image: "../../images/37.jpg",
+    image: "images/37.jpg",
     size: [34, 36, 38, 40, 42, 44, 46, 48],
     color: ["schwarz-wollweiß", "wollweiß", "rot"]
   },
@@ -423,7 +423,7 @@ export const items = [
     title: "Minirock in Velourlederoptik",
     description: "Schmal geschnittener Minirock",
     price: "42.99",
-    image: "../../images/38.jpg",
+    image: "images/38.jpg",
     size: [34, 36, 38, 40, 42],
     color: ["himbeere", "schwarz"]
   },
@@ -434,7 +434,7 @@ export const items = [
     description:
       "Mit dem Maxirock im Hippie-Look fühlt man sich einmalig gut gestylt. Dafür sorgt das spannende Alloverdruck-Design im Ethno-Look. Der kontrastfarbene Spitzenbesatz verleiht der trendigen Longform dieses leicht ausgestellten Rocks noch mehr Designstärke und der elastische Dehnbund garantiert eine optimale Passform. Damit nicht genug, setzt das weich fließende, elastische Jerseymaterial noch optimale Trageeigenschaften für tolle Outfitkreationen auf diesen ansprechenden Maxirock von Boysen's obendrauf. Besser geht's nicht!",
     price: "29.99",
-    image: "../../images/39.jpg",
+    image: "images/39.jpg",
     size: [34, 36, 38, 40, 42, 44, 48, 50, 52],
     color: ["rot-braun", "blau-weiß-schwarz"]
   },
@@ -445,7 +445,7 @@ export const items = [
     description:
       "Mit rosigem Alloverprint setzt dich das Umstandsshirt von Neun Monate auch in der Schwangerschaft wunderbar attraktiv in Szene. Der Gummizug an Rundhalsausschnitt und Saum sorgt für figurschmeichelnde Raffungen und tollen Sitz des Schwangerschafts-T-Shirts, der auch durch die angeschnittenen kurzen Ärmel charmant verstärkt wird.",
     price: "24.99",
-    image: "../../images/40.jpg",
+    image: "images/40.jpg",
     size: [34, 36, 38, 40, 42, 44],
     color: []
   },
@@ -456,7 +456,7 @@ export const items = [
     description:
       "Mit dem Limettenölreiniger lösen sie im Nu Fett und alle Arten von Schmutz. Verdünnt ist er ein Alleskönner, wenn es um die tägliche Hausarbeit und das Abwaschen und Reinigen von Flächen geht.",
     price: "74.99",
-    image: "../../images/41.jpg",
+    image: "images/41.jpg",
     size: [],
     color: ["silberfarben"]
   }
